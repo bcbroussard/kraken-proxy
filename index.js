459bf16ca031d34d9d22df59d422438c7671fb17
BC Broussard <bc.broussard@flyingcranes.com>
2015-02-07 20:17:45 -0800

    add CORS

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
-httpProxy.createServer({
+var proxy = httpProxy.createServer({
   // replace with remote destination api server
   target:'http://0.0.0.0:8080'
-}).listen(9000);
+});
+
+proxy.on('proxyRes', function(proxyRes, req, res, options) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+});
+
+proxy.listen(9000);
